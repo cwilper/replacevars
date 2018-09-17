f241e94c7ab6bc6915c0226b19bf8393ad9a865a
Chris Wilper <chris@atmire.com>
2018-09-16 23:18:19 -0400

    return instead of exiting when called as function

--- sourcing-example.sh
+++ sourcing-example.sh
@@ -9,3 +9,6 @@ last=Person
 
 # Prints "Hi Some Person"
 echo "$input" | replacevars -
+
+# Fails due to undefined variable and exits with code 42
+echo "{{meaning_of_life}}" | replacevars - || exit 42
